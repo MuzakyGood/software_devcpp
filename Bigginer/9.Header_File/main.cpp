d28da24dabcfd854a380c4dca48931430d2babea
Zach Noland <muzackyulum@gmail.com>
2024-07-10 15:01:22 +0700

    beta update

---
+++ Bigginer/9.Header_File/main.cpp
@@ -0,0 +1,14 @@
+/* 
+    Today i want show u header in C++
+    I sugest to use std:: cuz its standart in C++
+    In this learn i will show u header-guard file in C++
+*/
+#include <iostream>
+#include "include/add.h"
+
+int main(int argc, char const *argv[]){
+
+    std::cout << plus();
+
+    return 0;
+}
